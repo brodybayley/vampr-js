7d4606fefd461b3839564a323fd78c6be38a4ab7
Brody Bayley <brodybayley@gmail.com>
2021-02-20 00:16:20 +0000

    pushing through completed vampire.js

--- vampire.js
+++ vampire.js
@@ -16,18 +16,25 @@ class Vampire {
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
+
 
   /** Stretch **/
 
@@ -40,16 +47,6 @@ class Vampire {
 
   }
 }
-
-const original = new Vampire("Original", 1928);
-
-const ansel = new Vampire("Ansel", 1935);
-const bart = new Vampire("Bart", 1936);
-
-const elgort = new Vampire("Elgort", 1989);
-const sarah = new Vampire("Sara", 1990);
-
-const andrew = new Vampire("Andrew", 2020);
 
 
 module.exports = Vampire;
