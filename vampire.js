860eb29f8f3384acc1d181b3b55c531207ed87d8
Brody Bayley <brodybayley@gmail.com>
2021-02-20 01:17:35 +0000

    completed vampirewithname function

--- vampire.js
+++ vampire.js
@@ -16,34 +16,50 @@ class Vampire {
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
+    if (this.name === name) {
+      return this;
+    }
+    for (const baby of this.offspring) {
+      let vampire = baby.vampireWithName(name);
+      // baby.vampireWithName(name);
+      if (vampire !== null) {
+        return vampire;
+      }
+    }
+    return null;
   }
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
-    
+
   }
 
   // Returns an array of all the vampires that were converted after 1980
   get allMillennialVampires() {
-    
+
   }
 
   /** Stretch **/
@@ -64,10 +80,11 @@ const ansel = new Vampire("Ansel", 1935);
 const bart = new Vampire("Bart", 1936);
 
 const elgort = new Vampire("Elgort", 1989);
-const sarah = new Vampire("Sara", 1990);
+const sara = new Vampire("Sara", 1990);
 
 const andrew = new Vampire("Andrew", 2020);
 
 
 module.exports = Vampire;
 
+console.log(original.vampireWithName('Sara'));
